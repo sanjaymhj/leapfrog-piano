2ecc1010ece26aba882214246c9b563cb20e4e52
lkhanal56 <lkhanala56@gmail.com>
2016-01-03 17:37:51 +0545

    main function updated

--- js/Main.js
+++ js/Main.js
@@ -15,23 +15,6 @@ function Main() {
   }
   
  
-}
-
-function piano() {
-  var data = new Data();
-  var newKeys = new CreateKeys();
-  var bgAudio = new AudioInBackground();
-  var learn = new LearnPiano();
-  new Notes();
-  
-  var drum = false;
- 
-  var that = this;
-
-  for(var i = 0; i<Object.keys(data.noteList).length; i++){
-    newKeys.createNewKeys(data.noteList[i].name,data.noteList[i].notePitch, i);
-
-  }
-  
+}  
  
 }
